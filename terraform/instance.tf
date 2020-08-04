2bc837ce9b678313493e3bd464527374cc55a74e
projeto3 <42242436+projeto3@users.noreply.github.com>
2020-08-04 15:56:07 -0300

    Update instance.tf

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "wwws_jks_ci_cd" {
   ami           = "ami-01819babfeb403eca"
   instance_type = "t2.micro"
-//  subnet_id     = "subnet-b36ef198"
+  subnet_id     = "subnet-b36ef198"
   key_name	    = "aws_commvault"
 //  load_balancers = ["${aws_elb.my-elb.name}"]
 }

8c390a733f1e1ecbf5b49e1ebecf23f3855d0c2d
projeto3 <42242436+projeto3@users.noreply.github.com>
2020-08-04 15:19:10 -0300

    Update instance.tf

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "wwws_jks_ci_cd" {
-  ami           = "ami-6871a115"
+  ami           = "ami-01819babfeb403eca"
   instance_type = "t2.micro"
 //  subnet_id     = "subnet-b36ef198"
   key_name	    = "aws_commvault"
